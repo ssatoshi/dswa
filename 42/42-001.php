552e08a935b20d8b5931d764df023003d431248a
ssatoshi <ssatoshi>
2015-10-30 00:12:29 +0900

    updated .gitignore

---
+++ 42/42-001.php
@@ -0,0 +1,10 @@
+<?php
+$name = isset($_GET['name']) ? $_GET['name'] : '';
+if(! mb_check_encoding($name, 'Shift_JIS')){
+  die('');
+}
+$name = mb_convert_encoding($name, 'UTF-8', 'Shift_JIS');
+?>
+<body>
+name is <?php echo htmlspecialchars($name, ENT_NOQUOTES, 'UTF-8'); ?>.
+</body>
